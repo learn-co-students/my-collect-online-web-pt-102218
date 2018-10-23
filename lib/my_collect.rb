f80c20e3346b5500f6ec2296735af6d3bda4eb68
Johnathon Wood <jaywood128@gmail.com>
2018-10-23 21:23:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(languages)
+  i = 0
+  new_array = []
+  if languages.any?
 
+  while i < languages.length
+    yield (languages[i])
+    new_array << languages[i]
+    i += 1
+  end
 
+  new_array
+
+  else
+    "Collection is empty"
+  end
+end
+# my_collect(languages) {do |language|}
+#     puts languages
