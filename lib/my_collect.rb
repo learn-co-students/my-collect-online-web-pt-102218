6d3801c0aafbef7eecbec911a7b89fefbe7a90c6
Leslie Osorio <lesosorio17@gmail.com>
2018-11-01 21:18:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
+def my_collect(collection)
+counter = 0
+array = []
+while counter < collection.length
+  array << yield(collection[counter])
+  counter += 1
+end
+array
+end
 
+# my_collect(["ruby","javascript","python","objective-c"]) do |lang|
+#   lang.upcase
+# end
+#
+# my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) do |name|
+#   name.split(" ").first
+# end
