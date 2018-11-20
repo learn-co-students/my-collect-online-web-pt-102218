d3e782d0edde95d91e2cb8da893381e1fffd3361
Corina Talavera <corina.talavera@gmail.com>
2018-11-20 20:19:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,9 @@
-def my_collect
-["Tim Jones", "Tom Smith", "Jim Campagno"]
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push(yield array[i])
+    i += 1
+  end
+  new_array
+end
